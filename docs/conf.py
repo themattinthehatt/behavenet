e2200feb85c6c54e38592ab7a00e41954d9ca069
themattinthehatt <themattinthehatt@gmail.com>
2019-11-25 19:58:07 -0500

    updating sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,9 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_ops_theme'
-import sphinx_ops_theme
-html_theme_path = [sphinx_ops_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
