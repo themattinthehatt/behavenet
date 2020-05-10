29bc47ead2a0ecebf5c12081b14eb742505033cb
Eleanor Batty <eleanorbatty@Ellas-Mac.local>
2020-05-09 20:11:56 -0400

    loading pretrained weights

--- behavenet/fitting/ae_grid_search.py
+++ behavenet/fitting/ae_grid_search.py
@@ -11,6 +11,7 @@ from behavenet.fitting.utils import _print_hparams
 from behavenet.fitting.utils import build_data_generator
 from behavenet.fitting.utils import create_tt_experiment
 from behavenet.fitting.utils import export_hparams
+from behavenet.fitting.utils import load_pretrained_ae
 from behavenet.fitting.hyperparam_utils import get_all_params
 from behavenet.models import AE, ConditionalAE
 
@@ -63,6 +64,10 @@ def main(hparams, *args):
         raise NotImplementedError(
             'The model class "%s" is not currently implemented' % hparams['model_class'])
     model.to(hparams['device'])
+
+    ## Load pretrained weights if specified
+    model = load_pretrained_ae(model, hparams)
+
     model.version = exp.version
     torch_rnd_seed = torch.get_rng_state()
     hparams['training_rnd_seed'] = torch_rnd_seed
